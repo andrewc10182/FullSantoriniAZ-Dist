db8a90f3f378713f1958e96d5d1a55887d6c8a8c
andrewc10182 <46730534+andrewc10182@users.noreply.github.com>
2019-02-28 11:14:10 -0700

    Update player.py

--- FullSanto - Distributed/src/agent/player.py
+++ FullSanto - Distributed/src/agent/player.py
@@ -62,10 +62,247 @@ class GamePlayer:
 
             # this is for play_gui, not necessary when training.
             self.thinking_history[env.observation] = HistoryItem(action, policy, list(self.var_q[key]), list(self.var_n[key]))
-
+            mirror_x_env, mirror_x_policy = self.mirror_x(env, list(policy))
+            mirror_y_env, mirror_y_policy = self.mirror_y(env, list(policy))
+            mirror_xy_env, mirror_xy_policy = self.mirror_y(mirror_x_env, mirror_x_policy)
+            mirror_d_env, mirror_d_policy = self.mirror_d(env, list(policy))
+            mirror_dx_env, mirror_dx_policy = self.mirror_x(mirror_d_env, mirror_d_policy)
+            mirror_dy_env, mirror_dy_policy = self.mirror_y(mirror_d_env, mirror_d_policy)
+            mirror_dxy_env, mirror_dxy_policy = self.mirror_y(mirror_dx_env, mirror_dx_policy)
             self.moves.append([env.observation, list(policy)])
+            self.moves.append([mirror_x_env.observation, mirror_x_policy])
+            self.moves.append([mirror_y_env.observation, mirror_y_policy])
+            self.moves.append([mirror_xy_env.observation, mirror_xy_policy])
+            self.moves.append([mirror_d_env.observation, mirror_d_policy])
+            self.moves.append([mirror_dx_env.observation, mirror_dx_policy])
+            self.moves.append([mirror_dy_env.observation, mirror_dy_policy])
+            self.moves.append([mirror_dxy_env.observation, mirror_dxy_policy])
         return action
+    
+    def mirror_x(self, env, policy):
+        newenv = GameEnv().reset()
+        for i in range(5):
+            for j in range(5):
+                newenv.board[i][j] = env.board[4-i][j]
+        player = ['A', 'B', 'C']
+        if(env.player_turn() == Player.black): player = ['W', 'X', 'Y']
+        
+        newpolicy = [0]*128
+        for action in range(len(policy)):
+            if(policy[action]==0):
+                continue
+            else:
+                moving_worker = action // 64 # 0 if first worker, 1 if second worker
+                board = list(np.reshape(env.board,25))
+                worker1_pos = [i for i, n in enumerate(board) if n in player][0]
+                worker2_pos = [i for i, n in enumerate(board) if n in player][1]
 
+                if(moving_worker == 0):
+                    if(worker1_pos//5 < worker2_pos//5):
+                        moving_worker = 1-moving_worker
+                else:
+                    if(worker2_pos//5 > worker1_pos//5):
+                        moving_worker = 1-moving_worker
+                      
+                MoveCode = int((action%64)/8)+1
+                if(MoveCode>4): MoveCode+=1 #Changes 5 to 8 into 6 to 9
+
+                if(MoveCode == 1): MoveCode = 7 # text = text + 'Move ↖ '
+                elif(MoveCode == 2): MoveCode = 8 #text = text + 'Move ↑ '
+                elif(MoveCode == 3): MoveCode = 9 #text = text + 'Move ↗ '
+                elif(MoveCode == 7): MoveCode = 1
+                elif(MoveCode == 8): MoveCode = 2
+                elif(MoveCode == 9): MoveCode = 3
+
+                BuildCode = int(action%8)+1
+                if(BuildCode>4): BuildCode+=1
+                
+                if(BuildCode == 1): BuildCode = 7 #text = text + 'Build ↖'
+                elif(BuildCode == 2): BuildCode = 8 #text = text + 'Build ↑'
+                elif(BuildCode == 3): BuildCode = 9 #text = text + 'Build ↗'
+                elif(BuildCode == 7): BuildCode = 1 #text = text + 'Build ↙'
+                elif(BuildCode == 8): BuildCode = 2 #text = text + 'Build ↓'
+                elif(BuildCode == 9): BuildCode = 3 #text = text + 'Build ↘'
+
+                if(MoveCode>4): MoveCode-=1 #Changes 6 to 9 into 5 to 8
+                if(BuildCode>4): BuildCode-=1
+                newaction = (MoveCode-1)*8 + BuildCode - 1 + moving_worker*64
+                #print('Action converted from',action,'to',newaction)
+                newpolicy[newaction] = policy[action]
+
+        return newenv, newpolicy
+
+    def mirror_y(self, env, policy):
+        newenv = GameEnv().reset()
+        for i in range(5):
+            for j in range(5):
+                newenv.board[i][j] = env.board[i][4-j]
+        player = ['A', 'B', 'C']
+        if(env.player_turn() == Player.black): player = ['W', 'X', 'Y']
+        
+        newpolicy = [0]*128
+        for action in range(len(policy)):
+            if(policy[action]==0):
+                continue
+            else:
+                moving_worker = action // 64 # 0 if first worker, 1 if second worker
+                board = list(np.reshape(env.board,25))
+                worker1_pos = [i for i, n in enumerate(board) if n in player][0]
+                worker2_pos = [i for i, n in enumerate(board) if n in player][1]
+
+                if(worker1_pos//5==worker2_pos//5):
+                    moving_worker = 1-moving_worker
+                      
+                MoveCode = int((action%64)/8)+1
+                if(MoveCode>4): MoveCode+=1 #Changes 5 to 8 into 6 to 9
+
+                if(MoveCode == 1): MoveCode = 3 
+                elif(MoveCode == 4): MoveCode = 6 
+                elif(MoveCode == 7): MoveCode = 9 
+                elif(MoveCode == 3): MoveCode = 1
+                elif(MoveCode == 6): MoveCode = 4
+                elif(MoveCode == 9): MoveCode = 7
+
+                BuildCode = int(action%8)+1
+                if(BuildCode>4): BuildCode+=1
+                
+                if(BuildCode == 1): BuildCode = 3
+                elif(BuildCode == 4): BuildCode = 6 
+                elif(BuildCode == 7): BuildCode = 9 
+                elif(BuildCode == 3): BuildCode = 1 
+                elif(BuildCode == 6): BuildCode = 4 
+                elif(BuildCode == 9): BuildCode = 7 
+
+                if(MoveCode>4): MoveCode-=1 #Changes 6 to 9 into 5 to 8
+                if(BuildCode>4): BuildCode-=1
+                newaction = (MoveCode-1)*8 + BuildCode - 1 + moving_worker*64
+                #print('Action converted from',action,'to',newaction)
+                newpolicy[newaction] = policy[action]
+
+        return newenv, newpolicy
+
+    def d_new_pos(self, pos):
+        if pos==0: return 0
+        elif pos==1: return 5
+        elif pos==2: return 10
+        elif pos==3: return 15
+        elif pos==4: return 20
+        elif pos==5: return 1
+        elif pos==6: return 6
+        elif pos==7: return 11
+        elif pos==8: return 16
+        elif pos==9: return 21
+        elif pos==10: return 2
+        elif pos==11: return 7
+        elif pos==12: return 12
+        elif pos==13: return 17
+        elif pos==14: return 22
+        elif pos==15: return 3
+        elif pos==16: return 8
+        elif pos==17: return 13
+        elif pos==18: return 18
+        elif pos==19: return 23
+        elif pos==20: return 4
+        elif pos==21: return 9
+        elif pos==22: return 14
+        elif pos==23: return 19
+        elif pos==24: return 24
+
+    def first_worker_is_first(self, worker1_pos, worker2_pos):
+        if(worker1_pos//5 < worker2_pos//5):
+            return True
+        elif(worker1_pos < worker2_pos):
+            return True
+        return False
+
+    def mirror_d(self, env, policy):
+        newenv = GameEnv().reset()
+        for i in range(5):
+            for j in range(5):
+                newenv.board[i][j] = env.board[j][i]
+        player = ['A', 'B', 'C']
+        if(env.player_turn() == Player.black): player = ['W', 'X', 'Y']
+        
+        newpolicy = [0]*128
+        for action in range(len(policy)):
+            if(policy[action]==0):
+                continue
+            else:
+                moving_worker = action // 64 # 0 if first worker, 1 if second worker
+                board = list(np.reshape(env.board,25))
+                worker1_pos = [i for i, n in enumerate(board) if n in player][0]
+                worker2_pos = [i for i, n in enumerate(board) if n in player][1]
+
+                if(self.first_worker_is_first(self.d_new_pos(worker1_pos), self.d_new_pos(worker2_pos)) == False):
+                    moving_worker = 1-moving_worker
+                      
+                MoveCode = int((action%64)/8)+1
+                if(MoveCode>4): MoveCode+=1 #Changes 5 to 8 into 6 to 9
+
+                if(MoveCode == 2): MoveCode = 4 # text = text + 'Move ↖ '
+                elif(MoveCode == 3): MoveCode = 7 #text = text + 'Move ↑ '
+                elif(MoveCode == 6): MoveCode = 8 #text = text + 'Move ↗ '
+                elif(MoveCode == 4): MoveCode = 2
+                elif(MoveCode == 7): MoveCode = 3
+                elif(MoveCode == 8): MoveCode = 6
+
+                BuildCode = int(action%8)+1
+                if(BuildCode>4): BuildCode+=1
+                
+                if(BuildCode == 2): BuildCode = 4 #text = text + 'Build ↖'
+                elif(BuildCode == 3): BuildCode = 7 #text = text + 'Build ↑'
+                elif(BuildCode == 6): BuildCode = 8 #text = text + 'Build ↗'
+                elif(BuildCode == 4): BuildCode = 2 #text = text + 'Build ↙'
+                elif(BuildCode == 7): BuildCode = 3 #text = text + 'Build ↓'
+                elif(BuildCode == 8): BuildCode = 6 #text = text + 'Build ↘'
+
+                if(MoveCode>4): MoveCode-=1 #Changes 6 to 9 into 5 to 8
+                if(BuildCode>4): BuildCode-=1
+                newaction = (MoveCode-1)*8 + BuildCode - 1 + moving_worker*64
+                #print('Action converted from',action,'to',newaction)
+                newpolicy[newaction] = policy[action]
+
+        return newenv, newpolicy
+    
+    def policy_to_direct_text(self, action):
+        moving_worker = action // 64
+        text = 'Action '+str(action)+' '
+        text = text + 'Worker '+str(moving_worker)+' '
+        action = action % 64
+        
+        MoveCode = int(action/8)+1
+        if(MoveCode>4): MoveCode+=1 #Changes 5 to 8 into 6 to 9
+        if(MoveCode == 1):
+            text = text + 'Move ↖ '
+        if(MoveCode == 2):
+            text = text + 'Move ↑ '
+        if(MoveCode == 3):
+            text = text + 'Move ↗ '
+        if(MoveCode == 4):
+            text = text + 'Move ← '
+        if(MoveCode == 6):
+            text = text + 'Move → '
+        if(MoveCode == 7):
+            text = text + 'Move ↙ '
+        if(MoveCode == 8):
+            text = text + 'Move ↓ '
+        if(MoveCode == 9):
+            text = text + 'Move ↘ '
+
+        BuildCode = int(action%8)+1
+        if(BuildCode>4): BuildCode+=1
+
+        #print('BuildCode',BuildCode,'Pos i & j at ',Posi, Posj)
+        if(BuildCode == 1): text = text + 'Build ↖'
+        if(BuildCode == 2): text = text + 'Build ↑'
+        if(BuildCode == 3): text = text + 'Build ↗'
+        if(BuildCode == 4): text = text + 'Build ←'
+        if(BuildCode == 6): text = text + 'Build →'
+        if(BuildCode == 7): text = text + 'Build ↙'
+        if(BuildCode == 8): text = text + 'Build ↓'
+        if(BuildCode == 9): text = text + 'Build ↘'
+
+        return text
     def ask_thought_about(self, board) -> HistoryItem:
         return self.thinking_history.get(board)
 
