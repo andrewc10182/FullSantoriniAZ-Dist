0be9ea400e911dbfdc3e12371a4e209548a1c34a
andrewc10182 <46730534+andrewc10182@users.noreply.github.com>
2019-03-01 14:15:48 -0700

    Update evolver.py

--- FullSanto - Distributed/src/worker/evolver.py
+++ FullSanto - Distributed/src/worker/evolver.py
@@ -184,12 +184,11 @@ class EvolverWorker:
         total_steps += steps
         self.save_current_model()
         
-        # Remove the old next generation models after training the new next generation
-        try: self.remove_model(get_next_generation_model_dirs(self.config.resource)[0])
-        except: dummy=0
         if(len(self.dbx.files_list_folder('/model/next_generation/').entries)>1):
             self.dbx.files_delete('/model/next_generation/'+self.dbx.files_list_folder('/model/next_generation').entries[0].name)
             
+            # Assuming if there's a next_generation, there is one also in the docker folder - delete it
+            self.remove_model(get_next_generation_model_dirs(self.config.resource)[0])
         last_save_step = total_steps
 
     def load_play_data(self):
