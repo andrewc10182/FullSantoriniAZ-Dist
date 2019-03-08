4564d61620e3bc5feb7502e277868281e5d5b3a3
andrewc10182 <46730534+andrewc10182@users.noreply.github.com>
2019-03-08 10:36:26 -0700

    Update assistant.py

--- FullSanto - Distributed/src/worker/assistant.py
+++ FullSanto - Distributed/src/worker/assistant.py
@@ -285,32 +285,32 @@ class AssistantWorker:
         #for game_idx in range(1,self.config.eval.game_num+1):
         while(len(self.dbx.files_list_folder('/EvaluateWinCount').entries) < self.config.eval.game_num):
             ng_win, white_is_best = self.play_game(self.best_model, ng_model)
-            ##if ng_win is not None:
-            ##    results.append(ng_win)
-            ##    winning_rate = sum(results) / len(results)
-            ##if(ng_win==1 and white_is_best):
-            ##    print('Challenger Wins with Black.')#  Winning rate ',winning_rate)
-            ##elif(ng_win==1 and not white_is_best):
-            ##    print('Challenger Wins with White.')#  Winning rate ',winning_rate)
-            ##elif(ng_win==0 and white_is_best):
-            ##    print('Challenger Loses with Black.')#  Winning rate ',winning_rate)
-            ##elif(ng_win==0 and not white_is_best):
-            ##    print('Challenger Loses with White.')#  Winning rate ',winning_rate)
             
             # Save a "Win" File in Dropbox if win, and "Lose" File if lose
-            if(ng_win==1):
-                filename = 'win'+str(random.random()*200000//2)
-            else:
-                filename = 'lose'+str(random.random()*200000//2)
+            if(ng_win==1): 
+                if(white_is_best): filename = 'wb'+str(random.random()*2000000//2)
+                else: filename = 'ww'+str(random.random()*2000000//2)
+            else: 
+                if(white_is_best): filename = 'lb'+str(random.random()*2000000//2)
+                else: filename = 'lw'+str(random.random()*2000000//2)
+                
             res = self.dbx.files_upload(bytes('abc', 'utf8'), '/EvaluateWinCount/'+filename, dropbox.files.WriteMode.add, mute=True)
-
-            w = 0
-            l = 0
-            for entry in self.dbx.files_list_folder('//EvaluateWinCount').entries:
-                if(entry.name[0] == 'w'): w +=1
-                else: l += 1
-                #print(entry.name)
-            print('Cloud Records of Wins:',w,'Lose:',l,'Total:',w+l,'Current Rate:',w/(w+l))
+            
+            ww = 0
+            wb = 0
+            lw = 0
+            lb = 0
+            
+            try:
+                for entry in self.dbx.files_list_folder('//EvaluateWinCount').entries:
+                    if(entry.name[:2] == 'ww'): ww +=1
+                    elif(entry.name[:2] == 'wb'): wb += 1
+                    elif(entry.name[:2] == 'lw'): lw += 1
+                    elif(entry.name[:2] == 'lb'): lb += 1
+                temp = 'In '+str(ww+wb+lw+lb)+' Cloud Games, NG Wins '+str(ww)+' of '+str(ww+lw)+' (%.3f)%% games in White, ' + str(wb)+' of '+str(wb+lb)+' (%.3f)%% games in Black, '+ str(ww+wb)+' of '+str(ww+wb+lw+lb)+' (%.3f)%% games in Total.'
+                temp2 = (ww/(ww+lw)*100, wb/(wb+lb)*100, (ww+wb)/(ww+wb+lw+lb)*100)
+                print(temp % temp2)
+            except: pass
             
         return True
 
