23005ec67889ada6cde97215740dfbbd98aab327
andrewc10182 <46730534+andrewc10182@users.noreply.github.com>
2019-03-11 09:01:58 -0600

    Update normal.py

--- FullSanto - Distributed/src/configs/normal.py
+++ FullSanto - Distributed/src/configs/normal.py
@@ -33,7 +33,7 @@ class PlayConfig:
 
 class TrainerConfig:
     def __init__(self):
-        self.batch_size = 1024
+        self.batch_size = 2048 #1024
         self.epoch_to_checkpoint = 2
         self.start_total_steps = 0
         self.save_model_steps = 9999 # 150
